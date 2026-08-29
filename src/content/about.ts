d33806a6449fabd0c1d28cb7e4579b8792daa898
Aakash Wijesekara <aakashwije92@gmail.com>
2026-08-29 09:50:41 +0530

    enchance about section

--- src/content/about.ts
+++ src/content/about.ts
@@ -12,6 +12,40 @@ export const aboutStats: AboutStat[] = [
 
 export type AboutValue = { icon: IconName; title: string; desc: string };
 
+export type LeadershipMember = {
+  name: string;
+  title: string;
+  image: string | null;
+  linkedinUrl: string;
+};
+
+export const leadershipTeam: LeadershipMember[] = [
+  {
+    name: "Aakash Wijesekara",
+    title: "Co-Founder, CEO & Chief Technology Officer",
+    image: "/team/senior-management/Aakash.png",
+    linkedinUrl: "https://www.linkedin.com/in/aakash-wijesekara-611588318/",
+  },
+  {
+    name: "Thisen Bandara",
+    title: "Co-Founder & Chief Product Officer (CPO)",
+    image: "/team/senior-management/Thisen.jpeg",
+    linkedinUrl: "https://www.linkedin.com/in/thisen-bandara-048924359/",
+  },
+  {
+    name: "Ayora Fernando",
+    title: "Head of Software Operations & Delivery",
+    image: "/team/senior-management/Ayora.jpeg",
+    linkedinUrl: "https://www.linkedin.com/in/ayora-fernando-73b40134b/",
+  },
+  {
+    name: "Imesha Meegoda",
+    title: "Head of Marketing",
+    image: "/team/senior-management/imesha.jpeg",
+    linkedinUrl: "https://www.linkedin.com/in/imesha-meegoda-180b31352/",
+  },
+];
+
 export const aboutValues: AboutValue[] = [
   {
     icon: "precision_manufacturing",
